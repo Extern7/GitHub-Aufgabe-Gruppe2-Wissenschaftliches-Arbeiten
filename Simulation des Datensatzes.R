27477d19d49abd862ed7677761a42c18161b7a3b
Extern7 <119423471+Extern7@users.noreply.github.com>
2023-02-11 12:46:13 +0100

    Rahmen für das Simulationsskript
    Anfang unserer Arbeit an der Simulation. Auf dieses Skript sollen nur ich und Patrick zugreifen.

---
+++ Simulation des Datensatzes.R
@@ -0,0 +1,49 @@
+
+# Wissenschaftliches Arbeiten - Git Hub Projekt (Gruppe 2)
+
+## Aufgabe 1 - Datensimulation
+
+
+# Dataframe erstellen
+
+
+
+
+
+# Alter
+
+
+
+
+
+
+# Studienfach
+
+
+
+
+
+
+
+# Interesse an Mathematik
+
+
+
+
+
+
+
+# Interesse an Programmieren
+
+
+
+
+
+
+
+# Mathe-LK
+
+
+
+
+
