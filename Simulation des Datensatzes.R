c988d28bf3df2cb74c33088dae37661a99a17c66
Extern7 <119423471+Extern7@users.noreply.github.com>
2023-02-13 12:15:39 +0100

    Update #1 Simulation
    Leeren Dataframe erstellt; ID, Alter und Studienfach simuliert.
    Wahrscheinlichkeiten für "Studienfach" können auch gerne noch geändert werden

--- Simulation des Datensatzes.R
+++ Simulation des Datensatzes.R
@@ -4,28 +4,48 @@
 ## Aufgabe 1 - Datensimulation
 
 
-# Dataframe erstellen
+## Dataframe erstellen
+
+data <- data.frame("ID" = seq(1,100,1),
+                   "Alter" = rep(0,100),
+                   "Studienfach" = rep(0,100),
+                   "Interesse an Mathematik" = rep(0,100),
+                   "Interesse an Programmieren" = rep(0,100),
+                   "Mathe-LK" = rep(0,100))
+
+# Ich erstelle erstmal einen leeren Dataframe der die passende Form hat.
+# Nach und nach füllen wir dann die einzelnen Spalten.
+
+
+
+## Alter
+
+data$Alter <- rnorm(100, mean = 25, sd = 2)
+
+# Normalverteiltes Alter simuliert
+
+
+
+## Studienfach
+
+data$Studienfach <- sample(c("Statistik", "Data Science", "Mathe",
+                             "Informatik"), size = 100, replace = TRUE,
+                           prob = c(0.35, 0.35, 0.05, 0.25))
+  
+# Erst einmal mit Wahrscheinlichkeiten, die in etwa der Aufgabenstellung entsprechen
+# Wir können die gerne noch etwas anpassen, wenn du möchtest
+
+
+
+## Interesse an Mathematik
 
 
 
 
 
-# Alter
 
 
-
-
-
-
-# Studienfach
-
-
-
-
-
-
-
-# Interesse an Mathematik
+## Interesse an Programmieren
 
 
 
@@ -33,15 +53,7 @@
 
 
 
-# Interesse an Programmieren
-
-
-
-
-
-
-
-# Mathe-LK
+## Mathe-LK
 
 
 
