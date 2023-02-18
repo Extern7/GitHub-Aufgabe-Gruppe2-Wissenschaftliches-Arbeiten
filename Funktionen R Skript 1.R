9a104700c0a260a5b9631fb8bf45e6e1729f4294
Extern7 <119423471+Extern7@users.noreply.github.com>
2023-02-18 13:34:02 +0100

    Rahmen für das Funktionenskript 1

---
+++ Funktionen R Skript 1.R
@@ -0,0 +1,43 @@
+# Wissenschaftliches Arbeiten - Git Hub Projekt (Gruppe 2)
+
+## Aufgabe 3a - Funktionen Skript 1
+
+
+## Funktion a
+
+
+
+
+
+## Funktion b
+
+
+
+
+
+## Funktion c
+
+
+
+
+
+## Funktion d
+
+
+
+
+
+## Funktion e
+
+
+
+
+
+## Funktion f
+
+
+
+
+
+
+## Weitere Funktionen
