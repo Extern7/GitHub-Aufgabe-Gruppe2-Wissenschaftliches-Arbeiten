432a5cea7385a118978871dd0dd30dbd30c4785e
Me37127 <101261364+Me37127@users.noreply.github.com>
2023-02-23 14:40:45 +0100

    Update Analyse-Skript.R

--- Analyse-Skript.R
+++ Analyse-Skript.R
@@ -1,3 +1,31 @@
+##Analyse des Alters 
+dmetrische(data$Alter)
 
 
-dkat(Data$Alter)
+#Das Alter scheint um die 25 normalverteilt zu sein. Mit einer Spannweite
+#von 19 bis 30
+
+
+#Studienfach
+
+dkat(data$Studienfach)
+
+#Data Science und Statistik scheinen gleichverteilt zu sein.
+#Mathe hat nur einen kleinen Anteil
+
+
+
+
+#Studienfach ~ Interesse an Mathematik
+
+sum(data$Studienfach == "Mathe" & data$Interesse.an.Mathematik > 3)/
+  sum(data$Studienfach == "Mathe")
+#[1] 0.2
+
+sum(data$Studienfach == "Data Science" & data$Interesse.an.Mathematik > 3)/
+  sum(data$Studienfach == "Data Science")
+#[1] 0.5945946 
+
+#Kein logischer Zusammenhang, da nur 20 Prozent der Mathe Studierenden
+#mindestens eine 4 bei dem Interesse an Mathematik angegeben haben. 
+#Bei den Data Scientist sind es schon 60 Prozent
