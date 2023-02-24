cc4d1a549e14d8fe5f7dc073f6e43a1530317e19
Simon Kutzner <119423471+Extern7@users.noreply.github.com>
2023-02-24 18:12:02 +0100

    Update Funktionen R Skript 1 - Idee für "weitere Funktionen"
    Kommafehler korriegiert, Idee für eine weitere Barplot Funktion, die bei der Analyse sehr hilfreich sein kann. 
    Funktion ist nicht fertig aber eine Beispiel Idee habe ich schon eingefügt.

--- Funktionen R Skript 1.R
+++ Funktionen R Skript 1.R
@@ -48,7 +48,7 @@ dquan <- function(x){
   niedrig <- quantile(x, probs = 1/3)
   mittel <- quantile(x, probs = 2/3)
   hoch <- quantile(x, probs = 1)
-  barplot(c(niedrig, mittel, hoch), names = c("niedrig", "mittel", "hoch")),
+  barplot(c(niedrig, mittel, hoch), names = c("niedrig", "mittel", "hoch"))
   table(cut(x, breaks = c(0, niedrig, mittel, hoch), 
             labels = c("niedrig", "mittel", "hoch")))
 }
@@ -64,3 +64,26 @@ dquan <- function(x){
 
 
 ## Weitere Funktionen
+
+
+# Idee: Barplot der relative Häufigkeiten von Ausprägungen kategorieller Variablen in 
+# Abhängigkeit von den Interessen darstellt
+
+# beispielhaft:
+
+data <- read.csv("datensatz.csv")
+
+barplot(matrix(c(
+  sapply(1:7, function(x) sum(data$Studienfach[which(data$Interesse.an.Mathematik == x)] == "Data Science"))/
+    sum(data$Studienfach == "Data Science"),
+  sapply(1:7, function(x) sum(data$Studienfach[which(data$Interesse.an.Mathematik == x)] == "Informatik"))/
+    sum(data$Studienfach == "Informatik"),
+  sapply(1:7, function(x) sum(data$Studienfach[which(data$Interesse.an.Mathematik == x)] == "Mathe"))/
+    sum(data$Studienfach == "Mathe"),
+  sapply(1:7, function(x) sum(data$Studienfach[which(data$Interesse.an.Mathematik == x)] == "Statistik"))/
+    sum(data$Studienfach == "Statistik")), nrow = length(table(data$Interesse.an.Mathematik))),
+  beside = TRUE,
+  main = "Barplot", ylab = "relative Häufigkeiten", names.arg = c("Data Science", "Informatik",
+                                                                  "Mathe", "Statistik"))
+
+# Nächster Schritt: Diesen Barplot als Funktion verallgemeinern
