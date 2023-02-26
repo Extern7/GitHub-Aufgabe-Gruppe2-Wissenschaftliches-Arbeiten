2f05a7c8761d6b0c8aa5493a8ed6fe2bf9c8ddbd
Me37127 <101261364+Me37127@users.noreply.github.com>
2023-02-26 15:39:58 +0100

    Update Analyse-Skript.R
    Ergänzung zu ##Studienfach ~ Interesse an Mathematik

--- Analyse-Skript.R
+++ Analyse-Skript.R
@@ -1,22 +1,18 @@
 ##Analyse des Alters 
+
 dmetrische(data$Alter)
-
-
 #Das Alter scheint um die 25 normalverteilt zu sein. Mit einer Spannweite
 #von 19 bis 30
 
 
-#Studienfach
+##Studienfach
 
 dkat(data$Studienfach)
-
 #Data Science und Statistik scheinen gleichverteilt zu sein.
 #Mathe hat nur einen kleinen Anteil
 
 
-
-
-#Studienfach ~ Interesse an Mathematik
+##Studienfach ~ Interesse an Mathematik
 
 sum(data$Studienfach == "Mathe" & data$Interesse.an.Mathematik > 3)/
   sum(data$Studienfach == "Mathe")
@@ -29,3 +25,18 @@ sum(data$Studienfach == "Data Science" & data$Interesse.an.Mathematik > 3)/
 #Kein logischer Zusammenhang, da nur 20 Prozent der Mathe Studierenden
 #mindestens eine 4 bei dem Interesse an Mathematik angegeben haben. 
 #Bei den Data Scientist sind es schon 60 Prozent
+
+dkat_biv(data$Studienfach, data$Interesse.an.Mathematik)
+#Data Science und Statistik Studierende haben größeres Interesse an Mathematik (>= 6), als Studierende der Informatik
+#und Mathematik. Der Informatik Bereich sieht realistisch aus. Mathematik eher unrealistisch, da
+#die meisten Mathematikstudierenden nur "2" bei Interesse an Mathematik angegeben haben.
+
+dquan(data$Interesse.an.Mathematik)
+#Nur ca. 1/3 der Studierenden haben ein hohes Interesse an Mathematik, obwohl es sich grundsätzlich um Studiengänge mit einem
+#hohen Mathematikanteil handelt.
+
+
+##
+
+dbiv(data$Alter, data$Mathe.LK) 
+#Es gibt keine Person unter 21, welche kein Mathe-LK hatte
