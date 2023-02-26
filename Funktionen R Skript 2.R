01ce40c16a3fcbf4198fad2ce6c288b93c87c80c
Simon Kutzner <119423471+Extern7@users.noreply.github.com>
2023-02-26 19:52:08 +0100

    Update Funktionen R Skript 2 - dichkod()
    Hinzufügen der dichkod() Funktion

--- Funktionen R Skript 2.R
+++ Funktionen R Skript 2.R
@@ -1,1 +1,15 @@
 #Funktionen R Skript 2 für interne Funkitionen
+
+## dichkod()
+
+dichkod <- function(dich){
+  if(length(table(dich)) != 2){
+    stop("Input besitzt keine dichotomen Auspraegungen")
+  }
+  x <- dich
+  x[which(x == names(table(x))[1])] <- 0
+  x[which(x == names(table(x))[2])] <- 1
+  return(as.numeric(x))
+}
+
+# Funktion kodiert dichotome Variablen mit 0 und 1.  
