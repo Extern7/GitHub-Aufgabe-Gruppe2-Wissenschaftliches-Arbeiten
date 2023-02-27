10e60017a692c924c5e2bc9581f12f9d9a461418
merfi8 <122815912+merfi8@users.noreply.github.com>
2023-02-27 13:10:30 +0100

    Update Analyse-Skript.R

--- Analyse-Skript.R
+++ Analyse-Skript.R
@@ -3,28 +3,29 @@
 dmetrische(data$Alter)
 #Das Alter scheint um die 25 normalverteilt zu sein. Mit einer Spannweite
 #von 19 bis 30
-
+sd(data$Alter)
+#Mit einer Standardabweichung von ungefaehr 2.0 .
 
 ##Studienfach
 
 dkat(data$Studienfach)
-#Data Science und Statistik scheinen gleichverteilt zu sein.
-#Mathe hat nur einen kleinen Anteil
-
+#Data Science und Statistik scheinen gleichverteilt zu sein mit Anteilen von jeweils ca. 35%.
+#Informatik hat einen kleineren Anteil mit ca. 20%. 
+#Mathe hat den kleinsten Anteil mit ca. 5%
 
 ##Studienfach ~ Interesse an Mathematik
 
-sum(data$Studienfach == "Mathe" & data$Interesse.an.Mathematik > 3)/
-  sum(data$Studienfach == "Mathe")
-#[1] 0.2
+int.biv(data$Interesse.an.Mathematik, 3)
+#Nur 20 Prozent der Mathe Studierenden haben mindestens eine 4 bei dem Interesse an Mathematik angegeben.
+#Man sollte davon ausgehen, dass Mathe Studierende ein größeres Interesse an Mathe haben. 
+#Es liegen anscheinend zu wenig Daten vor.
+#Bei den Data Scientist sind es schon 60 Prozent, bei den Statistikern 49 Prozent und bei den Informatikern 52 Prozent. 
+#Es ist nirgendwo ein klarer Zusammenhang zwischen Studienfach und Interesse an Mathematik erkennbar. 
 
-sum(data$Studienfach == "Data Science" & data$Interesse.an.Mathematik > 3)/
-  sum(data$Studienfach == "Data Science")
-#[1] 0.5945946 
-
-#Kein logischer Zusammenhang, da nur 20 Prozent der Mathe Studierenden
-#mindestens eine 4 bei dem Interesse an Mathematik angegeben haben. 
-#Bei den Data Scientist sind es schon 60 Prozent
+int.biv(data$Interesse.an.Programmieren, 3)
+#Es ist zu erkennen, dass ein Großteil der Informatiker ein Interesse an Programmieren hat.
+#Es haben nämlich 71 Prozent der Informatiker mindestens eine 4 angegeben.
+#Die restlichen Studiengaenge haben ein gleichverteiltes Interesse.
 
 dkat_biv(data$Studienfach, data$Interesse.an.Mathematik)
 #Data Science und Statistik Studierende haben größeres Interesse an Mathematik (>= 6), als Studierende der Informatik
